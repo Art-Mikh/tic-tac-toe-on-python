27d6e4edf7b4bbd5c8092412c265917065721c71
Artem <139917899+Art-Mikh@users.noreply.github.com>
2023-07-19 22:04:53 +0300

    Tic-tac-toe-on-python
    A tic-tac-toe console application built using Python classes with documentation. PEP 257 – Docstring

--- class_game_with_computer.py
+++ class_game_with_computer.py
@@ -1,11 +1,18 @@
+"""
+File containing the class GameWithComputer,
+containing methods that implement the game of two people
+"""
 from class_main_game_logic import MainGameLogic, Label
 from class_main_game_logic import GameStatus, PlayerTurn
 
 
 class GameWithComputer(MainGameLogic):
+    """
+    The class contains methods that implement the game for two people.
+    """
     def main_logic(self) -> None:
         game: dict = self.game_init()
-        while game['game_status'] == GameStatus.сontinues.value:
+        while game['game_status'] == GameStatus.CNTINUES.value:
             game['field_value_fill'] = self.choose_player_move(game)
             game = self.move_search(game)
             game['sign_of_move'] = self.get_player_symbol(game['player_turn'])
@@ -13,9 +20,18 @@ class GameWithComputer(MainGameLogic):
         self.ending_game(game)
 
     def move_search(self, game: dict) -> dict:
-        # 3. Если компьютер нашел куда сделать ход, то играем. Если нет, то ничья.
+        """
+        Method for finding a move If the computer has found where
+        to a move, then we play. If not, then a draw.
+
+        Args:
+            game (dict): dictionary with current game statuses
+
+        Returns:
+            dict: dictionary with assigned draw values
+        """
         found_game_move = dict(game)
-        if found_game_move['field_value_fill'] != Label.none.value:
+        if found_game_move['field_value_fill'] != Label.NONE.value:
             found_game_move['game_map'] = self.make_move(found_game_move)
             found_game_move['winner'] = self.get_game_result(found_game_move)
             found_game_move['game_status'] = self.check_victory(found_game_move['winner'])
@@ -24,27 +40,63 @@ class GameWithComputer(MainGameLogic):
         return found_game_move
 
     def check_victory(self, winner: str) -> bool:
-        if winner != Label.none.value:
-            return GameStatus.finished.value
-        else:
-            return GameStatus.сontinues.value
+        """
+        Sets the flag for the end
+        of the game if a winner is found
+
+        Args:
+            winner (str): game winner flag
+
+        Returns:
+            bool: Game end flag
+        """
+        if winner != Label.NONE.value:
+            return GameStatus.FINISHED.value
+        return GameStatus.CNTINUES.value
 
     def assign_status_draw(self, game: dict) -> dict:
+        """
+        Filling in statuses for a draw
+
+        Args:
+            game (dict): dictionary with current game statuses
+
+        Returns:
+            dict: dictionary with affixed draw statuses
+        """
         game_result: dict = dict(game)
         print('Ничья!')
-        game_result['game_status'] = GameStatus.finished.value
+        game_result['game_status'] = GameStatus.FINISHED.value
         game_result['winner'] = "Ничья"
         return game_result
 
     def choose_player_move(self, game: dict) -> int:
-        if game['player_turn'] == PlayerTurn.player_1.value:
-            return self.get_input_from_user(game)
-        else:
-            print("Компьютер делает ход: ")
-            return self.AI_choice_move(game)
+        """
+        Changing the player's turn
 
-    def AI_choice_move(self, game: dict) -> int:
-        step: int = Label.none.value
+        Args:
+            game (dict): dictionary with current game statuses
+
+        Returns:
+            int: the position of the move made by the
+                player or the computer
+        """
+        if game['player_turn'] == PlayerTurn.PLAYER_1.value:
+            return self.get_input_from_user(game)
+        print("Компьютер делает ход: ")
+        return self.ai_choice_move(game)
+
+    def ai_choice_move(self, game: dict) -> int:
+        """
+        Method that implements AI for choosing a move
+
+        Args:
+            game (dict): with current game statuses
+
+        Returns:
+            int: found computer move
+        """
+        step: int = Label.NONE.value
         step = self.complete_your_line(game)
         step = self.block_another_line(game, step)
         step = self.add_second_0(game, step)
@@ -53,70 +105,180 @@ class GameWithComputer(MainGameLogic):
         return step
 
     def complete_your_line(self, game: dict) -> int:
+        """
+        Checking for filling inyour line,
+        consisting of the same character
+
+        Args:
+            game (dict): dictionary with current game statuses
+
+        Returns:
+            int: cell number of the occupied field
+        """
         return self.check_line(game, 2, 0)
 
     def block_another_line(self, game: dict, step: int) -> int:
-        if step == Label.none.value:
+        """
+        Blocks someone else's line by placing his
+        symbol if there is a line of two enemy symbols
+
+        Args:
+            game (dict): with current game statuses
+            step (int): Previous move value
+
+        Returns:
+            int: cell number of the occupied field
+        """
+        if step == Label.NONE.value:
             return self.check_line(game, 0, 2)
         return step
 
     def add_second_0(self, game: dict, step: int) -> int:
-        if step == Label.none.value:
+        """
+        The method appends the second 0
+
+        Args:
+            game (dict): dictionary with current game statuses
+            step (int): Previous move value
+
+        Returns:
+            int: cell number of the occupied field
+        """
+        if step == Label.NONE.value:
             return self.check_line(game, 1, 0)
         return step
 
     def take_center(self, game: dict, step: int) -> int:
+        """
+        Method occupies the center of the field
+
+        Args:
+            game (dict): dictionary with current game statuses
+            step (int): Previous move value
+
+        Returns:
+            int: cell number of the occupied field
+        """
         if (
-            step == Label.none.value and
-            game['game_map'][4] != Label.x.value and
-            game['game_map'][4] != Label.o.value
+            step == Label.NONE.value and
+            game['game_map'][4] != Label.X.value and
+            game['game_map'][4] != Label.O.value
         ):
             return 5
         return step
 
     def take_first_slot(self, game: dict, step: int) -> int:
+        """
+        first cell method
+
+        Args:
+            game (dict): dictionary with current game statuses
+            step (int): Previous move value
+
+        Returns:
+            int: cell number of the occupied field
+        """
         if (
-            step == Label.none.value and
-            game['game_map'][0] != Label.x.value and
-            game['game_map'][0] != Label.o.value
+            step == Label.NONE.value and
+            game['game_map'][0] != Label.X.value and
+            game['game_map'][0] != Label.O.value
         ):
             return 1
         return step
 
-    # Искусственный интеллект: поиск линии с нужным количеством X и O на победных линиях
-    def check_line(self, game: dict, sum_X: int, sum_O: int) -> int:
+    def check_line(self, game: dict, sum_x: int, sum_o: int) -> int:
+        """
+        Method for finding a line with the right amount of X and O on winning lines
+
+        Args:
+            game (dict): dictionary with current game statuses
+            sum_x (int): quantity X
+            sum_o (int): quantity 0
+
+        Returns:
+            int: returns the computer's progress
+        """
         game_map: list = game['game_map']
-        step = Label.none.value
+        step = Label.NONE.value
         for line in game['winning_lines']:
             sum_labels: dict = self.find_number_labels(game_map, line)
-            step = self.choose_move(game_map, line, sum_labels, sum_X, sum_O, step)
+            choose_list = self.create_dictionary_for_move_selection(game_map, line, sum_labels)
+            step = self.choose_move(choose_list, sum_x, sum_o, step)
         return step
 
     def find_number_labels(self, game_map: list, checked_line: list) -> dict:
+        """
+        The method counts the number of X's and 0's in the field line
+
+        Args:
+            game_map (list): current playing field
+            checked_line (list): tested line
+
+        Returns:
+            dict: dictionary with counted number of x's and 0's
+        """
         sum_labels: dict = self.get_dictionary_sum_labels()
         for j in range(0, 3):
-            if game_map[checked_line[j]] == Label.o.value:
+            if game_map[checked_line[j]] == Label.O.value:
                 sum_labels['o'] += 1
-            if game_map[checked_line[j]] == Label.x.value:
+            if game_map[checked_line[j]] == Label.X.value:
                 sum_labels['x'] += 1
         return sum_labels
 
     def get_dictionary_sum_labels(self) -> dict:
+        """
+        Returns a dictionary to count the number of x and 0
+
+        Returns:
+            dict: dictionary to count the number of x's and 0's
+        """
         return {'x': 0, 'o': 0}
 
+    def create_dictionary_for_move_selection(
+        self, game_map: list, checked_line: list, sum_labels: dict
+    ) -> list:
+        """
+        The method creates a list to reduce the number of containers
+
+        Args:
+            game_map (list): current game map
+            checked_line (list): tested line
+            sum_labels (dict): dictionary sum of x and 0
+
+        Returns:
+            list: list to reduce the number of parameters
+        """
+        return [game_map, checked_line, sum_labels]
+
     def choose_move(self,
-                    game_map: list,
-                    checked_line: list,
-                    sum_labels: dict,
-                    sum_X: int,
-                    sum_O: int,
+                    choose_list: list,
+                    sum_x: int,
+                    sum_o: int,
                     step: int) -> int:
+        """
+        Method selects the computer's move
+
+        Args:
+            choose_list (list): contains parameters:
+                game_map: list
+                checked_line: list
+                sum_labels: dict
+            sum_x (int): quantity X
+            sum_o (int): quantity o
+            step (int):
+
+        Returns:
+            int: found computer move
+        """
+        game_map: list = choose_list[0]
+        checked_line: list = choose_list[1]
+        sum_labels: dict = choose_list[2]
         ret_step = step
-        if sum_labels['o'] == sum_O and sum_labels['x'] == sum_X:
+        if sum_labels['o'] == sum_o and sum_labels['x'] == sum_x:
             for j in range(0, 3):
                 if (
-                    game_map[checked_line[j]] != Label.o.value
-                    and game_map[checked_line[j]] != Label.x.value
+                    game_map[checked_line[j]] != Label.O.value
+                    and game_map[checked_line[j]] != Label.X.value
                 ):
                     ret_step = game_map[checked_line[j]]
         return ret_step
--- class_main_game_logic.py
+++ class_main_game_logic.py
@@ -1,52 +1,90 @@
+"""
+The file with the abstract class
+MainGameLogic and contains the
+main methods of the game tic-tac-toe
+"""
 from abc import ABC, abstractmethod
 from enum import Enum
 
 
 class GameStatus(Enum):
-    сontinues: bool = True
-    finished: bool = False
+    """
+    Class for storing game status.
+    Is the game in progress or over
+    """
+    CNTINUES: bool = True
+    FINISHED: bool = False
 
 
 class PlayerTurn(Enum):
-    player_1: bool = True
-    player_2: bool = False
+    """
+    class containing the player's turn queue flag
+    """
+    PLAYER_1: bool = True
+    PLAYER_2: bool = False
 
 
 class Label(Enum):
-    x: str = ("X",)
-    o: str = ("0",)
-    none: str = ""
+    """
+    Class containing characters to move
+    """
+    X: str = ("X",)
+    O: str = ("0",)
+    NONE: str = ""
 
 
 class PlayerMoveValidation(Enum):
-    correct_value: bool = True
-    invalid_value: bool = False
+    """
+    Class contains markers of correctness of the player's move
+    """
+    CORRECT_VALUE: bool = True
+    INVALID_VALUE: bool = False
 
 
 class MainGameLogic(ABC):
+    """
+    The abstract class contains the core
+    logic of the tic-tac-toe game
+    """
     def __init__(self):
         self.main_logic()
 
     @abstractmethod
     def main_logic(self) -> None:
-        pass
+        """
+        The basic logic of tic-tac-toe.
+        This method will be different for
+        playing with a computer and for two
+        """
 
     def game_init(self) -> dict:
+        """
+        Dictionary initialization method
+        containing all the start flags
+        and labels for the game
+        """
         return {
             'game_map': self.map_initialization(),
             'winning_lines': self.initialize_winning_positions(),
-            'game_status': GameStatus.сontinues.value,
-            'player_turn': PlayerTurn.player_1.value,
-            'sign_of_move': Label.x.value,
+            'game_status': GameStatus.CNTINUES.value,
+            'player_turn': PlayerTurn.PLAYER_1.value,
+            'sign_of_move': Label.X.value,
             'winner': '',
             'field_value_fill': ''
         }
 
     def map_initialization(self) -> list:
+        """
+        Method returning an empty playing field
+        """
         game_map = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         return game_map
 
     def initialize_winning_positions(self) -> list:
+        """
+        Method that returns all possible
+        paylines to win the game
+        """
         winning_lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -60,120 +98,271 @@ class MainGameLogic(ABC):
         return winning_lines
 
     def make_move(self, game: dict) -> list:
+        """
+        The method marks the player's turn on the map
+
+        Args:
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            list: updated game map with marked moves
+        """
         ret_map: list = game['game_map']
         ind = ret_map.index(game['field_value_fill'])
         ret_map[ind] = game['sign_of_move']
         return ret_map
 
     def get_game_result(self, game: dict) -> str:
-        winner: str = Label.none.value
+        """
+        the method returns the winner of
+        the game if one of the players won
+
+        Args:
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            str: game winner player symbol
+        """
+        winner: str = Label.NONE.value
         for line in game['winning_lines']:
             winner = self.check_win(winner, game['game_map'], line)
         return winner
 
     def check_win(self, winner: str, game_map: list, winning_line: list) -> str:
+        """
+        The method checks if a winning
+        position has been created on
+        one of the winning lines
+
+        Args:
+            winner (str): symbol of the winner
+            game_map (list): current game map
+            winning_line (list): winning line to check
+
+        Returns:
+            str: _description_
+        """
         winner_symbol: str = winner
         if (
             game_map[winning_line[0]]
             == game_map[winning_line[1]]
             == game_map[winning_line[2]]
-            == Label.x.value
+            == Label.X.value
         ):
-            winner_symbol = Label.x.value
+            winner_symbol = Label.X.value
         elif (
             game_map[winning_line[0]]
             == game_map[winning_line[1]]
             == game_map[winning_line[2]]
-            == Label.o.value
+            == Label.O.value
         ):
-            winner_symbol = Label.o.value
+            winner_symbol = Label.O.value
         return winner_symbol
 
     def get_input_from_user(self, game: dict) -> int:
+        """
+        The method returns the number
+        of the cell in which the move was made
+
+        Args:
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            int: the number of the cell in which the move was made
+        """
         self.print_map(game['game_map'])
-        valid_flag = PlayerMoveValidation.invalid_value.value
-        while valid_flag != PlayerMoveValidation.correct_value.value:
+        valid_flag = PlayerMoveValidation.INVALID_VALUE.value
+        while valid_flag != PlayerMoveValidation.CORRECT_VALUE.value:
             symbol = input("Ход игрока " + game['sign_of_move'][0] + ": ")
             valid_flag = self.check_player_move(symbol, game)
         return int(symbol)
 
     def check_player_move(self, symbol: str, game: dict) -> bool:
-        valid_flag = PlayerMoveValidation.invalid_value.value
+        """
+        Сhecking the player's move
+
+        Args:
+            symbol (str): character entered by the player
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            bool: flag - the result of checking
+                the character entered by the player
+        """
+        valid_flag = PlayerMoveValidation.INVALID_VALUE.value
         valid_flag = self.number_check(symbol, game)
         return valid_flag
 
     def number_check(self, symbol: str, game: dict) -> bool:
+        """
+        the character entered by the player
+        is checked for the presence of a number
+
+        Args:
+            symbol (str): character entered by the player
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            bool: flag - the result of checking
+                the character entered by the player
+        """
         if symbol.isdigit():
             numder = int(symbol)
             return self.validating_number_within_field(numder, game)
-        else:
-            print("Введите номер поля для хода! Введите номер другой ячейки:")
-            return PlayerMoveValidation.invalid_value.value
+        print("Введите номер поля для хода! Введите номер другой ячейки:")
+        return PlayerMoveValidation.INVALID_VALUE.value
 
     def validating_number_within_field(self, numder: int, game: dict) -> bool:
+        """
+        The method checks if the number
+        entered by the player is between 1 and 9
+
+        Args:
+            numder (int): character entered by the player
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            bool: flag - the result of checking
+                the character entered by the player
+        """
         if 0 < numder < 10:
             return self.cell_not_busy(numder, game['game_map'])
-        else:
-            print("Введите номер поля для хода от 1 до 9!:")
-            return PlayerMoveValidation.invalid_value.value
+        print("Введите номер поля для хода от 1 до 9!:")
+        return PlayerMoveValidation.INVALID_VALUE.value
 
     def cell_not_busy(self, numder: int, game_map: list) -> bool:
+        """
+        Checking if a cell is already occupied or not
+
+        Args:
+            numder (int): character entered by the player
+            game (dict): dictionary containing all game tags
+
+        Returns:
+            bool: flag - the result of checking
+                the character entered by the player
+        """
         if (
-            game_map[numder - 1] != Label.x.value and
-            game_map[numder - 1] != Label.o.value
+            game_map[numder - 1] != Label.X.value and
+            game_map[numder - 1] != Label.O.value
         ):
-            return PlayerMoveValidation.correct_value.value
-        else:
-            print("Эта клетка занята! Введите номер другой ячейки: ")
-            return PlayerMoveValidation.invalid_value.value
+            return PlayerMoveValidation.CORRECT_VALUE.value
+        print("Эта клетка занята! Введите номер другой ячейки: ")
+        return PlayerMoveValidation.INVALID_VALUE.value
 
     def get_player_symbol(self, player_turn: bool) -> str:
-        if player_turn == PlayerTurn.player_1.value:
-            return Label.o.value
-        else:
-            return Label.x.value
+        """
+        Returns the character of the player in his turn
+
+        Args:
+            player_turn (bool): player turn flag
+
+        Returns:
+            str: player symbol
+        """
+        if player_turn == PlayerTurn.PLAYER_1.value:
+            return Label.O.value
+        return Label.X.value
 
     def change_player_turn(self, player_turn: bool) -> bool:
-        if player_turn == PlayerTurn.player_1.value:
-            return PlayerTurn.player_2.value
-        else:
-            return PlayerTurn.player_1.value
+        """
+        Passes the current turn to the next player
+
+        Args:
+            player_turn (bool): player turn flag
+
+        Returns:
+            bool: next player turn flag
+        """
+        if player_turn == PlayerTurn.PLAYER_1.value:
+            return PlayerTurn.PLAYER_2.value
+        return PlayerTurn.PLAYER_1.value
 
     def ending_game(self, game: dict) -> None:
+        """
+        The method of displaying information at the end of the game
+
+        Args:
+            game (dict): dictionary containing all game tags
+        """
         self.print_map(game['game_map'])
         print("Победил", self.change_symbol(game['winner']))
 
     def print_map(self, game_map: list) -> None:
+        """
+        Playing field display method
+
+        Args:
+            game_map (list): dictionary containing all game tags
+        """
         out_map = self.change_output_map(game_map)
         self.print_first_line_map(out_map)
         self.print_second_line_map(out_map)
         self.print_third_line_map(out_map)
 
     def change_output_map(self, game_map: list) -> list:
+        """
+        The method changes the playing field
+        for pretty output. The method changes
+        tuples to string characters
+
+        Args:
+            game_map (list): Game card for withdrawal
+
+        Returns:
+            list: new map to display on the screen
+        """
         return [
             self.change_symbol(item)
             for item in game_map
         ]
 
     def change_symbol(self, symbol: tuple) -> str:
-        if symbol == Label.x.value:
+        """
+        Method reverses the player's
+        symbol to reverse the player's turn order
+
+        Args:
+            symbol (tuple): current game symbol
+
+        Returns:
+            str: new game symbol
+        """
+        if symbol == Label.X.value:
             return "X"
-        elif symbol == Label.o.value:
+        if symbol == Label.O.value:
             return "0"
-        else:
-            return symbol
+        return symbol
 
     def print_first_line_map(self, out_map: list) -> None:
+        """
+        Displays the first line of the map
+
+        Args:
+            out_map (list): game map
+        """
         print(out_map[0], end=" ")
         print(out_map[1], end=" ")
         print(out_map[2])
 
     def print_second_line_map(self, out_map: list) -> None:
+        """
+        Displays the second line of the map
+
+        Args:
+            out_map (list): game map
+        """
         print(out_map[3], end=" ")
         print(out_map[4], end=" ")
         print(out_map[5])
 
     def print_third_line_map(self, out_map: list) -> None:
+        """
+        Displays the third line of the map
+
+        Args:
+            out_map (list): game map
+        """
         print(out_map[6], end=" ")
         print(out_map[7], end=" ")
         print(out_map[8])
